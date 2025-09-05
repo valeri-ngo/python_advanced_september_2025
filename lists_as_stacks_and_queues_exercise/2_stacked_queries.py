f5e3c304f93517807b74db601b77c2bafae843c5
valeri_ngo <sacredlie@abv.bg>
2025-09-05 23:13:35 +0300

    Stacks and Queues exercises.

---
+++ lists_as_stacks_and_queues_exercise/2_stacked_queries.py
@@ -0,0 +1,16 @@
+integer = int(input())
+
+stack = []
+
+functions = {
+    '1': lambda x: stack.append(int(x)),
+    '2': lambda: stack.pop() if stack else None,
+    '3': lambda: print(max(stack)) if stack else None,
+    '4': lambda: print(min(stack)) if stack else None
+}
+
+for _ in range(integer):
+    query = input().split()
+    functions[query[0]](*query[1:])
+
+print(', '.join([str(x) for x in reversed(stack)]))
