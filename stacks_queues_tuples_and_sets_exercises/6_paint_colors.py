675673c7df787d3f002b02c174a357ffa5a1ad15
valeri_ngo <sacredlie@abv.bg>
2025-09-21 18:12:03 +0300

    Stacks, Queues, Tuples and Sets exercises.

---
+++ stacks_queues_tuples_and_sets_exercises/6_paint_colors.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+color_string = deque(input().split())
+
+main_colors = ["red", "blue", "yellow"]
+secondary_colors = {
+    "orange" : ["red", "yellow"],
+    "purple" : ["red", "blue"],
+    "green" : ["yellow", "blue"]
+}
+
+collected_colors = []
+
+while color_string:
+
+    first_str = color_string.popleft()
+    last_str = color_string.pop() if color_string else ""
+
+    for color in (first_str + last_str, last_str + first_str):
+
+        if color in main_colors or color in secondary_colors:
+            collected_colors.append(color)
+            break
+
+    else:
+
+        if len(first_str) > 1:
+            color_string.insert(len(color_string) // 2, first_str[:-1])
+
+        if len(last_str) > 1:
+            color_string.insert(len(color_string) // 2, last_str[:-1])
+
+for color in collected_colors:
+    if color in secondary_colors:
+        for el in secondary_colors[color]:
+            if el not in collected_colors:
+                collected_colors.remove(color)
+                break
+
+print(collected_colors)
