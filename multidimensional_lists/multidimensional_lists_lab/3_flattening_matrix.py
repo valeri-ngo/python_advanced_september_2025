89a6cf61d55e82960fe2acc4083d2ae04c69c54f
valeri_ngo <sacredlie@abv.bg>
2025-09-22 12:58:20 +0300

    Multidimensional Lists lab.

--- multidimensional_lists/multidimensional_lists_lab/3_flattening_matrix.py
+++ multidimensional_lists/multidimensional_lists_lab/3_flattening_matrix.py
@@ -4,6 +4,5 @@ matrix = []
 
 for _ in range(rows):
     cols = [int(x) for x in input().split(", ")]
-    matrix.append(cols)
-flattened_matrix = [num for row in matrix for num in row]
-print(flattened_matrix)
+    matrix.extend(cols)
+print(matrix)
