f17c5404c367e1eb19e9540802c133ebc2659f0d
valeri_ngo <sacredlie@abv.bg>
2025-09-23 23:02:04 +0300

    Multidimensional Lists exercises.

--- multidimensional_lists/multidimensional_lists_exercises/04_maximal_sum.py
+++ multidimensional_lists/multidimensional_lists_exercises/04_maximal_sum.py
@@ -1,21 +1,22 @@
-row, col = [int(x) for x in input().split()]
+rows, cols = [int(x) for x in input().split()]
+matrix = [[int(x) for x in input().split()] for _ in range(rows)]
 
-matrix = []
+max_sum = -float("inf")
+max_row = 0
+max_col = 0
 
-for _ in range(row):
-    row_data = [int(x) for x in input().split()]
-    matrix.append(row_data)
+for row in range(rows - 2):
+    for col in range(cols - 2):
+        curr_sum = 0
+        for r in range(row, row + 3):
+            for c in range(col, col + 3):
+                curr_sum += matrix[r][c]
+        if curr_sum > max_sum:
+            max_sum = curr_sum
+            max_row = row
+            max_col = col
 
-best_sum = float('-inf')
-best_row = best_col = 0
+print(f"Sum = {max_sum}")
 
-for row_index in range(row - 2):
-    for col_index in range(col - 2):
-        block_sum = sum(matrix[row_index + r][col_index + c] for r in range(3) for c in range(3))
-        if block_sum > best_sum:
-            best_sum = block_sum
-            best_row, best_col = row_index, col_index
-
-print(f"Sum = {best_sum}")
-for r in range(3):
-    print(*[matrix[best_row + r][best_col + c] for c in range(3)])
+max_submatrix = [matrix[r][max_col:max_col + 3] for r in range(max_row, max_row + 3)]
+[print(*row) for row in max_submatrix]
