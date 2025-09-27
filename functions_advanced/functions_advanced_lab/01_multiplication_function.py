31bdce5db17808bfe36e5e47db51a1aae3f061c5
valeri_ngo <sacredlie@abv.bg>
2025-09-27 21:51:22 +0300

    Functions Advanced lab.

---
+++ functions_advanced/functions_advanced_lab/01_multiplication_function.py
@@ -0,0 +1,9 @@
+def multiply(*nums):
+    result = 1
+    for el in nums:
+        result *= el
+    return result
+
+print(multiply(1, 4, 5))
+print(multiply(4, 5, 6, 1, 3))
+print(multiply(2, 0, 1000, 5000))
