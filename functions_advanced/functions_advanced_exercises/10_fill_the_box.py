ffab839fead52889774520a1b0392271881c8b53
valeri_ngo <sacredlie@abv.bg>
2025-09-29 00:46:42 +0300

    Functions Advanced exercises.

--- functions_advanced/functions_advanced_exercises/10_fill_the_box.py
+++ functions_advanced/functions_advanced_exercises/10_fill_the_box.py
@@ -1,16 +1,12 @@
 def fill_the_box(height, length, width, *args):
     v = length * width * height
-    left = 0
     for cubes in args:
         if cubes == "Finish":
             break
         if v:
             v -= cubes
-        else:
-            left += cubes
-
-    if left:
-        return f"No more free space! You have {left} more cubes."
+    if v < 0:
+        return f"No more free space! You have {abs(v)} more cubes."
     return f"There is free space in the box. You could put {abs(v)} more cubes."
 
 print(fill_the_box(2, 8, 2, 2, 1, 7, 3, 1, 5, "Finish"))
