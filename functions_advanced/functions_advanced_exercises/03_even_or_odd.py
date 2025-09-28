7f4148b1917e7c240b4f7317f5a9151bdc9ed031
valeri_ngo <sacredlie@abv.bg>
2025-09-28 17:15:29 +0300

    Functions Advanced exercises.

---
+++ functions_advanced/functions_advanced_exercises/03_even_or_odd.py
@@ -0,0 +1,10 @@
+def even_odd(*args):
+    command = args[-1]
+    number = args[:-1]
+    if command == "even":
+        return [x for x in number if x % 2 == 0]
+    else:
+        return [x for x in number if x % 2 != 0]
+
+print(even_odd(1, 2, 3, 4, 5, 6, "even"))
+print(even_odd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "odd"))
