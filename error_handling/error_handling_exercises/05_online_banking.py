34efd4a826c01f7405460a4e25f29548dfa35341
valeri_ngo <sacredlie@abv.bg>
2025-10-01 23:14:37 +0300

    Error handling exercises.

--- error_handling/error_handling_exercises/05_online_banking.py
+++ error_handling/error_handling_exercises/05_online_banking.py
@@ -11,6 +11,12 @@ class MoneyIsNegativeError(Exception):
     pass
 
 def send_money(money_, curr_code, balance, actual_code_, age_, minimum_age):
+    """
+    A function for sending money to a friend.
+    Checking if the age restriction is valid.
+    Checks if the balance is enough else raises an exception.
+    Checks if the PIN code is correct else raises an exception.
+    """
     if age_ < minimum_age:
         raise UnderageTransactionError("You must be 18 years or older to perform online transactions")
     if money_ <= 0:
@@ -27,6 +33,11 @@ def send_money(money_, curr_code, balance, actual_code_, age_, minimum_age):
         raise MoneyNotEnoughError("Insufficient funds for the requested transaction")
 
 def receive_money(added_money, balance):
+    """
+    A function for receiving salary at the end of the month.
+    If statement for checking if the balance is below zero.
+    Else prints how much money are added to the bank account
+    """
     if added_money < 0:
         raise MoneyIsNegativeError("The amount of money cannot be a negative number")
     else:
@@ -36,21 +47,26 @@ def receive_money(added_money, balance):
 
 account_details = input().split(", ")
 actual_code = int(account_details[0])
-initial_balance = int(account_details[1])
+initial_balance = float(account_details[1])
 age = int(account_details[2])
 MINIMUM_AGE = 18
+# User input split by a comma and a space:
+# PIN code -> actual_code, bank balance -> initial_balance, current age -> age
 
 while True:
     command = input().split("#")
+    # Read commands split by "#"
+    # Send Money: money_sent#code
+    # Receive Money: salary (50% is being added into the bank)
 
     if command[0] == "End":
         break
 
     if command[0] == "Send Money":
-        money_sent, code = int(command[1]), int(command[2])
+        money_sent, code = float(command[1]), int(command[2])
         initial_balance = send_money(money_sent, code, initial_balance, actual_code, age, MINIMUM_AGE)
 
     elif command[0] == "Receive Money":
-        salary = int(command[1])
-        money_added = salary // 2
+        salary = float(command[1])
+        money_added = salary / 2
         initial_balance = receive_money(money_added, initial_balance)
