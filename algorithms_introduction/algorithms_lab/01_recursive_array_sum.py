04ffc75f07484f057ee03078121aae366a173f89
valeri_ngo <sacredlie@abv.bg>
2025-10-21 22:20:58 +0300

    Algorithms Introduction.

---
+++ algorithms_introduction/algorithms_lab/01_recursive_array_sum.py
@@ -0,0 +1,8 @@
+def calc_sum(numbers, idx = 0):
+    if idx == (len(numbers) -1):
+        return numbers[idx]
+
+    return numbers[idx] + calc_sum(numbers, idx + 1)
+
+num = [int(x) for x in input().split()]
+print(calc_sum(num))
